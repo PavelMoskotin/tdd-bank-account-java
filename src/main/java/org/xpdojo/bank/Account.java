8616f8cb1fc4b4c7934c9bd6bfc7e6f836d7124d
PavelMoskotin <pavel.moskotin@gmail.com>
2020-07-16 15:10:08 +0100

    simplify Account class

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -2,11 +2,7 @@ package org.xpdojo.bank;
 
 public class Account {
 
-    private Long accountNumber;
-
     private int balance;
-
-    private boolean isAccountOpen;
 
     public Account() {
     }
@@ -15,26 +11,8 @@ public class Account {
         this.balance = balance;
     }
 
-    public Account(Long accountNumber, int balance) {
-        this.accountNumber = accountNumber;
-        this.balance = balance;
-        this.isAccountOpen = true;
-    }
-
-    public Long getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(Long accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
     public int getBalance() {
         return balance;
-    }
-
-    public void setBalance(int balance) {
-        this.balance = balance;
     }
 
     public void deposit(int amount) {
