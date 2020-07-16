4285dbcf7ebc51d85ca5082d5ef721ed8424acb1
PavelMoskotin <pavel.moskotin@gmail.com>
2020-07-16 14:51:00 +0100

    first test and deposit/withdraw functionality

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,50 @@
 package org.xpdojo.bank;
 
 public class Account {
+
+    private Long accountNumber;
+
+    private int balance;
+
+    private boolean isAccountOpen;
+
+    public Account() {
+    }
+
+    public Account(int balance) {
+        this.balance = balance;
+    }
+
+    public Account(Long accountNumber, int balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.isAccountOpen = true;
+    }
+
+    public Long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(Long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(int amount) {
+        this.balance += amount;
+    }
+
+    public void withdraw(int amount) {
+        if (this.balance >= amount ) {
+            this.balance -= amount;
+        }
+        // TODO add Exception raised
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -8,8 +8,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
+    public void newAccountHasZeroBalance() {
+        Account account = new Account();
+        assertThat(account.getBalance()).isEqualTo(0);
+    }
+
+    @Test
     public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+        Account account = new Account();
+        account.deposit(10);
+        assertThat(account.getBalance()).isEqualTo(10);
+    }
+
+    @Test
+    public void withdrawWorksWithSufficientBalance() {
+        Account account = new Account(20);
+        account.withdraw(10);
+        assertThat(account.getBalance()).isEqualTo(10);
+    }
+
+    @Test
+    public void withdrawDoesNotWorkWithInsufficientBalance() {
+        Account account = new Account(10);
+        account.withdraw(20);
+        assertThat(account.getBalance()).isEqualTo(10);
+        // TODO add exception raised
     }
 }
